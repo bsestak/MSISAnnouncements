8a83570576566ac11905d28a07863b1ae61d8f85
Brady Sestak <brady.sestak@okstate.edu>
2020-05-02 19:54:54 -0500

    worked on offline service worker

--- MSISDepartmentApp/sw.js
+++ MSISDepartmentApp/sw.js
@@ -1,11 +1,49 @@
-﻿self.addEventListener('install', evt => {
-    console.log('service worker has been instaled');
+﻿const staticCacheName = 'site-static-V1';
+const assets = [
+    '/Home.aspx',
+    '/Account.aspx',
+    '/SignIn.aspx',
+    '/SignOut.aspx',
+    '/NewEvent.aspx',
+    '/Scripts/app.js',
+    '/Content/AppCSS.css',
+    '/Content/OSU1.css',
+    '/Content/OSU2.css',
+    '/Content/OSU3.css',
+    '/Content/OSU4.css',
+    '/Scripts/jquery-3.4.1.min.js',
+    '/Scripts/bootstrap.min.js',
+    '/Content/bootstrap.min.css',
+    '/images/OSULogo.png'
+];
+
+
+self.addEventListener('install', evt => {
+    evt.waitUntil(
+        caches.open(staticCacheName).then(Cache => {
+            //console.log('caching shell assets')
+            Cache.addAll(assets);
+        })
+    );
 });
 
 self.addEventListener('activate', evt => {
-    console.log('service worker has been activated');
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName)
+                .map(key => caches.delete(key))
+            )
+        })
+    );
+    //console.log('service worker has been activated');
 });
 
 self.addEventListener('fetch', evt => {
-    console.log('fetch event',evt);
+    //console.log('Fetching',evt);
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request);
+        })
+    )
 });
